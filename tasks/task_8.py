68716656493288744cf7f4d6ec8be419bad8bf64
Shoxrux Yuldashov <deagleshox@mail.ru>
2024-01-07 08:28:37 +0000

    Проверка ДЗ | 10. Работа со списками
    Проверена: №8

--- tasks/task_8.py
+++ tasks/task_8.py
@@ -1,14 +1,19 @@
 line = input("Введите ваш текст: ").split()
 min_word = ""
 max_len = 0
-min_len = 0
+min_len = 0  # здесь ошибка
 max_word = ""
 for x in line:
     if len(x) > max_len:
         max_len = len(x)
         max_word = x
-    elif len(x) < max_len:
+    elif len(x) < max_len:  # и здесь
         min_len = len(x)
         min_word = x
 print(max_word)
 print(min_word)
+
+# Test: Дом перед персонажем резко обваливается
+
+# Общее замечание: В коде одно и то же много раз
+# повторяется (len(элемент) вынесите это в переменную.
