cbcc4f16c802e0956e585f97a2e80920d17cb75f
DarkWell <darkwell463@gmail.com>
2024-01-08 19:08:08 +0300

    Решение V10 Домашнее задание | 10. Работа со списками Решение задач - 6, 8, 10, 14

---
+++ tasks/task_10.py
@@ -0,0 +1,14 @@
+all_fruits: list[str, ...] = ["Гранат", "Ананас", "Слива", "Апельсин", "Виноград",
+                              "Киви", "Яблоко", "Манго", "Абрикос", "Грейпфрукт"]
+i = 1
+fruits: list = []
+tropical_fruits: list[str, ...] = [x for x in all_fruits]
+
+
+for x in all_fruits:
+    if i % 2 == 0:
+        tropical_fruits.append(x)
+    else:
+        fruits.append(x)
+    i += 1
+print(f"Фрукты: {fruits}\nТропические фрукты: {tropical_fruits}")
--- tasks/task_8.py
+++ tasks/task_8.py
@@ -1,19 +1,5 @@
 line = input("Введите ваш текст: ").split()
-min_word = ""
-max_len = 0
-min_len = 0  # здесь ошибка
-max_word = ""
-for x in line:
-    if len(x) > max_len:
-        max_len = len(x)
-        max_word = x
-    elif len(x) < max_len:  # и здесь
-        min_len = len(x)
-        min_word = x
-print(max_word)
-print(min_word)
-
+line.sort(key=len, reverse=False)
+print(f"Самое длинное слово в предложении: {line[-1]}"
+      f"\nСамое короткое слово в предложении: {line[0]}")
 # Test: Дом перед персонажем резко обваливается
-
-# Общее замечание: В коде одно и то же много раз
-# повторяется (len(элемент) вынесите это в переменную.
