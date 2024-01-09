87c159a8f28cb02795df91325f82fd06b925fff1
DarkWell <darkwell463@gmail.com>
2024-01-09 19:35:05 +0300

    Решение V10 Домашнее задание | 10. Работа со списками Решение задач - 10

--- tasks/task_10.py
+++ tasks/task_10.py
@@ -1,17 +1,7 @@
 all_fruits: list[str, ...] = ["Гранат", "Ананас", "Слива", "Апельсин", "Виноград",
                               "Киви", "Яблоко", "Манго", "Абрикос", "Грейпфрукт"]
-i = 1
-fruits: list = []
-tropical_fruits: list[str, ...] = [x for x in all_fruits]
 
-for x in all_fruits:
-    if i % 2 == 0:
-        tropical_fruits.append(x)
-    else:
-        fruits.append(x)
-    i += 1
+fruits: list[str, ...] = all_fruits[0::2]
+tropical_fruits: list[str, ...] = all_fruits[1::2]
+
 print(f"Фрукты: {fruits}\nТропические фрукты: {tropical_fruits}")
-
-# Код слишком громоздкий, задачу можно решить в пару строк кода.
-
-# Подсказка: используйте срезы.
--- tasks/task_6.py
+++ tasks/task_6.py
@@ -1,22 +1,18 @@
 from random import randint
-
-# 1. Укажите аннотации типов для всех идентификаторов.
-# example_list = [11, 1, 22, 33, 14, 0, 13, 22, 11, 22, 9, 33]
+# example_list: list[int, ...] = [11, 1, 22, 33, 14, 0, 13, 22, 11, 22, 9, 33]
 
 # Список из 15 случайных чисел в диапазоне от 1 до 10
-example_list = [randint(1, 10) for _ in range(15)]
+example_list: list[int, ...] = [randint(1, 10) for _ in range(15)]
 print('Before:', example_list)
 
-new_list = []
-i = 0
+new_list: list = []
+i: int = 0
 while i < len(example_list):
     if example_list[i] not in new_list:
         new_list.append(example_list[i])
         i += 1
-    # здесь можно было просто использовать блок else
     else:
         del example_list[i]
 
 print('After:', example_list)
 
-# Все хорошо, не забудьте указать аннотации и убрать мои комментарии.
