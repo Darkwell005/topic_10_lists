b10505d96cd9d0f17ef7c3d09bd0dbb9e9fadf00
Shoxrux Yuldashov <deagleshox@mail.ru>
2024-01-14 11:44:18 +0000

    Проверка ДЗ | 10. Работа со списками
    - Правильно: №12, №20
    - Неправильно:
    - Улучшены: №19, №20
    - На доработку:

--- tasks/task_19.py
+++ tasks/task_19.py
@@ -1,10 +1,14 @@
 hint: str = "Введите числа, разделенные пробелами: "
 nums: list[int, ...] = [int(n) for n in input(hint).split()]
+
 length: int = len(nums)
 new_list: list[int, ...] = []
 for i in range(length):
     for j in range(length):
-        if ((nums[i] == nums[j] and i != j) and
-                nums[i] not in new_list):
+        if nums[i] == nums[j] and i != j:
             new_list.append(nums[i])
+            break
+
 print(*new_list)
+
+# 1 9 7 3 6 2 7 3 4 3
--- tasks/task_20.py
+++ tasks/task_20.py
@@ -1,3 +1,19 @@
 hint: str = "Введите числа, разделенные пробелами: "
 nums: list[int, ...] = [int(n) for n in input(hint).split()]
 
+length: int = len(nums)
+for i in range(length):
+    left_neighbor: int = nums[i - 1]
+    right_neighbor: int = nums[(i + 1) % length]
+    print(left_neighbor + right_neighbor, end=' ')
+
+# Option 2
+print()
+print(
+    *[
+        nums[i - 1] + nums[(i + 1) % length]
+        for i in range(length)
+    ]
+)
+
+# 4 2 7 5 8 3 13
